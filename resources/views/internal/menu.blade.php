8d8c9e7f639e359aadd117a1889e04718486a6ae
Artem Ivanov <aivanov@vic-insurance.ru>
2022-02-17 12:56:55 +0300

    Minor correction in Dashboard

--- resources/views/internal/menu.blade.php
+++ resources/views/internal/menu.blade.php
@@ -7,17 +7,14 @@
         </a>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+            <li><span class="nav-link px-2 link-secondary"> </span></li>
+            <li><a href="#" class="nav-link px-2 link-dark"> </a></li>
         </ul>
 
         <div class="col-md-2 text-end">
         <form method="POST" action="/internal/logout" class="mt-10">
             @csrf
-            <button type="submit" class="btn btn-outline-primary me-2">Logout</button>
+            <button type="submit" class="btn btn-outline-primary me-2">Выйти</button>
         </form>
         </div>
     </header>
