c9de9c36aa592045b6d99b7e4234865926b85df8
Artem Ivanov <aivanov@vic-insurance.ru>
2022-05-30 16:20:59 +0300

    WIP

--- app/Console/Commands/Probe.php
+++ app/Console/Commands/Probe.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
+use Carbon\CarbonImmutable;
 use orangedata\orangedata_client;
 
 class Probe extends Command
@@ -46,50 +47,73 @@ class Probe extends Command
             'period_text' => 'Январь 2023'
         ];
 
-        $orangeData = new orangedata_client(
-            [
-                'inn' => env('ORANGEDATA_INN'),
-                'api_url' => env('ORANGEDATA_URL'),
-                'sign_pkey' => storage_path('app/orangedata/private_key.pem'),
-                'ssl_client_key' => storage_path('app/orangedata/client.key'),
-                'ssl_client_crt' => storage_path('app/orangedata/client.crt'),
-                'ssl_ca_cert' => storage_path('app/orangedata/cacert.pem'),
-                'ssl_client_crt_pass' => env('ORANGEDATA_PASS'),
-            ]
-        );
+        $record = [];
+        try {
+            $requestTime = CarbonImmutable::now();
+            $record['request_time'] = $requestTime->format('c');
 
-        if (App::environment('local')) {
-            $orangeData->is_debug();
-        }
+            $orangeData = new orangedata_client(
+                [
+                    'inn' => env('ORANGEDATA_INN'),
+                    'api_url' => env('ORANGEDATA_URL'),
+                    'sign_pkey' => storage_path('app/orangedata/private_key.pem'),
+                    'ssl_client_key' => storage_path('app/orangedata/client.key'),
+                    'ssl_client_crt' => storage_path('app/orangedata/client.crt'),
+                    'ssl_ca_cert' => storage_path('app/orangedata/cacert.pem'),
+                    'ssl_client_crt_pass' => env('ORANGEDATA_PASS'),
+                ]
+            );
 
-        $orangeData->create_order([
-            'id' => random_int(1, PHP_INT_MAX),
-            'type' => 1,
-            'customerContact' => $accrual['email'],
-            'taxationSystem' => 0,
-            'key' => env('ORANGEDATA_INN'),
-            'callbackUrl' => route('orangedata'),
-        ])
-            ->add_position_to_order([
-                'quantity' => '1',
-                'price' => $accrual['sum'],
-                'tax' => 1,
-                'text' => "Квитанция по лицевому счету {$accrual['account']} за {$accrual['period_text']}",
-                'paymentMethodType' => 4,
-                'paymentSubjectType' => 13,
-                'supplierInfo' => [
-                    'phoneNumbers' => ['+74956486777'],
-                    'name' => 'А101-Комфорт',
-                ],
-                'supplierINN' => env('ORANGEDATA_INN'),
+            if (App::environment('local')) {
+                $orangeData->is_debug();
+            }
+
+            $orangeData->create_order([
+                'id' => random_int(1, PHP_INT_MAX),
+                'type' => 1,
+                'customerContact' => $accrual['email'],
+                'taxationSystem' => 0,
+                'key' => env('ORANGEDATA_INN'),
+                'callbackUrl' => route('orangedata'),
             ])
-            ->add_payment_to_order([
-                'type' => 16,
-                'amount' => $accrual['sum'],
-            ]);
+                ->add_position_to_order([
+                    'quantity' => '1',
+                    'price' => $accrual['sum'],
+                    'tax' => 1,
+                    'text' => "Квитанция по лицевому счету {$accrual['account']} за {$accrual['period_text']}",
+                    'paymentMethodType' => 4,
+                    'paymentSubjectType' => 13,
+                    'supplierInfo' => [
+                        'phoneNumbers' => ['+74956486777'],
+                        'name' => 'А101-Комфорт',
+                    ],
+                    'supplierINN' => env('ORANGEDATA_INN'),
+                ])
+                ->add_payment_to_order([
+                    'type' => 16,
+                    'amount' => $accrual['sum'],
+                ]);
 
-        $result = $orangeData->send_order();
-        echo 'SEND ORDER' . PHP_EOL;
-        var_dump($result);
+            $record['request'] = $orangeData->get_order();
+            $result = $orangeData->send_order();
+            $responseTime = CarbonImmutable::now();
+
+            echo 'SEND ORDER' . PHP_EOL;
+            var_dump($result);
+        } catch (\Throwable $th) {
+            $response['result_code'] = 23625490;
+            $response['result_message'] = $th->getMessage();
+            $response['exception'] = [
+                'message' => $th->getMessage(),
+                'code' => $th->getCode(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+            ];
+        } finally {
+            $record['response'] = $response;
+            $record['response_time'] = $responseTime->format('c');
+            $record['elapsed'] = $responseTime->floatDiffInSeconds($requestTime);
+            Log::info('outgoing-orangedata', $record);
+        }
     }
 }
