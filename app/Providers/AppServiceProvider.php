204e81b6b7fdc3cccaca58c344568f3111061457
Artem Ivanov <aivanov@vic-insurance.ru>
2022-06-17 14:14:54 +0300

    Improve and test the A101->sendCheque() method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\UniOne\UniOne;
 use MongoDB\Client;
+use orangedata\orangedata_client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,18 @@ class AppServiceProvider extends ServiceProvider
     {
         app()->singleton(UniOne::class, function () {
             return new UniOne(config('services.unione.api_key'));
+        });
+
+        app()->singleton(orangedata_client::class, function () {
+            return new orangedata_client([
+                'inn' => config('services.orangedata.inn'),
+                'api_url' => config('services.orangedata.url'),
+                'sign_pkey' => storage_path('app/orangedata/private_key.pem'),
+                'ssl_client_key' => storage_path('app/orangedata/client.key'),
+                'ssl_client_crt' => storage_path('app/orangedata/client.crt'),
+                'ssl_ca_cert' => storage_path('app/orangedata/cacert.pem'),
+                'ssl_client_crt_pass' => config('services.orangedata.pass'),
+            ]);
         });
 
         app()->singleton(Client::class, function () {
