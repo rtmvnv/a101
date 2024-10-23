c6744564fcb3936b109a22da172ce81bf3be8558
Artem Ivanov <aivanov@vic-insurance.ru>
2024-10-23 17:07:53 +0300

    Temporary disable payments on overhauls

--- resources/views/mail_overhaul/html.blade.php
+++ resources/views/mail_overhaul/html.blade.php
@@ -2,5 +2,5 @@
 @include('mail_overhaul/estate')
 @include('mail_overhaul/table')
 @include('mail_overhaul/a101_app')
-@include('mail_overhaul/button')
+<!-- @ include('mail_overhaul/button') -->
 @include('mail_overhaul/footer')
